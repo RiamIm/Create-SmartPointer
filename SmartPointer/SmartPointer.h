477f89756b90c70d441b56c4478fb9778d67043f
RiamIm <167727018+RiamIm@users.noreply.github.com>
2026-03-20 11:47:04 +0900

    Add project files.

---
+++ SmartPointer/SmartPointer.h
@@ -0,0 +1,69 @@
+﻿#pragma once
+#include <utility>
+#include <memory>
+
+namespace hwang {
+	template <typename T>
+	class unique_ptr {
+	public:
+		unique_ptr(T* raw)
+			: raw_(raw) {
+		}
+
+		~unique_ptr() {
+			if (raw_)
+				delete raw_;
+		}
+
+		// ===== 복사 생성 방지 =====
+		unique_ptr(const unique_ptr&) = delete;
+		unique_ptr& operator=(const unique_ptr&) = delete;
+
+		// 소유권 이동 생성
+		unique_ptr(unique_ptr&& other) noexcept {
+			raw_ = other.raw_;
+			other.raw_ = nullptr;
+		}
+
+		// 소유권 이동 대입
+		unique_ptr& operator=(unique_ptr&& other) noexcept {
+			// 자기 자신 대입 방지
+			if (this != &other) {
+				delete raw_;
+				raw_ = other.raw_;
+				other.raw_ = nullptr;
+			}
+
+			return*this;
+		}
+
+		// 주소 반환
+		T* get() const { return raw_; }
+
+		// 소유권 포기
+		T* release() {
+			auto* temp = raw_;
+			raw_ = nullptr;
+			return temp;
+		}
+
+		// 소유권 해제
+		void reset(T* p = nullptr) {
+			delete raw_;
+			raw_ = p;
+		}
+
+		// ===== 포인터 기본 연산자 =====
+		T& operator* () const { return *raw_; }
+		T* operator-> () const { return raw_; }
+
+	private:
+		T* raw_;
+	};
+
+	// make_unique 헬퍼 함수
+	template<typename T, typename... Args>
+	unique_ptr<T> make_unique(Args&&... args) {
+		return unique_ptr<T>(new T(std::forward<Args>(args)...));
+	}
+}
---
+++ SmartPointer/main.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <memory>
+
+#include "SmartPointer.h"
+
+struct Dog {
+	std::string name;
+	int age;
+};
+
+int main() {
+	std::cout << "===== unique_ptr =====\n";
+
+	hwang::unique_ptr<Dog> dogPtr(new Dog());
+	dogPtr->age = 10;
+	dogPtr->name = "개";
+
+	std::cout << dogPtr->age << '\n';
+	std::cout << dogPtr->name << '\n';
+
+	hwang::unique_ptr<int> ptr(new int(10));
+	int* raw = ptr.release();
+	std::cout << *raw << '\n';
+	delete raw; // 소유권 이전 -> 직접 delete
+
+	hwang::unique_ptr<int> ptr2(new int(10));
+	ptr2.reset(new int(20));
+	std::cout << *ptr2 << '\n';
+	ptr2.reset();
+	std::cout << ptr2.get() << '\n';
+	
+	std::cout << "===== make_unique 함수 사용 ===== \n";
+	auto a = hwang::make_unique<int>(10);
+	auto b = hwang::make_unique<Dog>("Buddy", 10);
+
+	std::cout << *a << '\n';
+	std::cout << b->name << '\n';
+
+	return 0;
+}
